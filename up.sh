5388fcdd7367d0fc25b810dbd9d8f2a3cd557b5a
zzigo <lucianoazzigotti@gmail.com>
2024-05-19 05:02:48 +0200

    atlichn

---
+++ up.sh
@@ -0,0 +1,4 @@
+cp -r /Users/zztt/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/zzttobsidian/public/ ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/quartz/content/
+git add .
+git commit -m "atlichn"
+git push https://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@github.com/zzigo/conblogqfig.git
